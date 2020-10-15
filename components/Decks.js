f65306cff8a4a0eeb78ed4522901f13222497a4b
Sabrina Dowla <yourGitHub@email.com>
2020-10-14 19:06:24 -0500

    Updated Files

--- components/Decks.js
+++ components/Decks.js
@@ -11,7 +11,7 @@ class Decks extends Component {
 
   handleNewCard = () => {
     this.props.navigation.navigate("AddNewCard", {
-      title: this.props.route.itemId
+      title: this.props.route.params
       // decks: this.props.decks
     });
   };
@@ -31,27 +31,27 @@ class Decks extends Component {
 
   handleDeleteDeck = () => {
     const { removeDeck, navigation, decks } = this.props;
-    const { itemId } = this.props.route.params;
-    const deck = decks[itemId];
-    const id = deck.title;
-    removeDeck(id);
-    removeDeckAsync(id);
-    navigation.goBack();
+    const { itemId } = this.props.route.params.itemId;
+    //const deck = decks[itemId];
+    //const id = deck.title;
+    removeDeck(this.props.route.params.itemId);
+    removeDeckAsync(this.props.route.params.itemId);
+    //navigation.goBack();
   };
 
   render() {
     const { navigation, decks } = this.props;
-    // const { itemId } = this.props.route.params;
-    const { itemId } = this.props.route;
+    //const { itemId } = this.props.route.params;
+    //const { itemId } = this.props.route.params.itemId;
 
-    totalCardCount: decks[itemId] && decks[itemId].questions.length;
+    // const cardCount = decks[itemId] && decks[itemId].questions.length;
 
     //const cardCount = decks[itemId] ? decks[itemId].questions.length : null;
 
     return (
       <View>
-        <Text> {itemId}</Text>
-        <Text>{totalCardCount}</Text>
+        <Text> {}</Text>
+        <Text>{} cards.</Text>
         <Button title="New Card" onPress={this.handleNewCard} />
         <Button title="Quiz" onPress={this.handleQuiz} />
         <Button title="Delete Deck" onPress={this.handleDeleteDeck} />
